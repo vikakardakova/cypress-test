838fc94d93b8eca49dc8160de5308d11c803860a
vikakardakova <vika2893@gmail.com>
2023-09-25 16:17:59 +0300

    few fixes

--- cypress/e2e/first.spec.js
+++ cypress/e2e/first.spec.js
@@ -15,7 +15,7 @@ describe ("First Suite", () => {
         cy.get("[data-name='chevron-down']").should("exist").click();
         cy.contains('Dark').click()
         cy.contains('Forms').should('have.css', 'background-color')
-        cy.wrap(colors.Dark).should("equal",'background-color')
+        cy.wrap(colors.Dark).should("equal","rgb(26, 33, 56)")
        
 
         // cy.contains('Forms').click()
@@ -26,43 +26,45 @@ describe ("First Suite", () => {
 
 
 
-    // it("second task", () => {
-    //     cy.visit ("/pages")
-    //     // cy.get("[title='Layout']").click()
-    //     cy.get('.menu-title').contains('Layout').click()
-    //     cy.contains('Accordion').click()
-    //     cy.contains ('A nebula is an interstellar cloud of dust, hydrogen').should('be.hidden')
-    //     cy.contains('Toggle First Item').click({force:true});
-    //     cy.contains ('A nebula is an interstellar cloud of dust, hydrogen').should('be.visible')
+    it("second task", () => {
+        cy.visit ("/pages")
+        // cy.get("[title='Layout']").click()
+        cy.get('.menu-title').contains('Layout').click()
+        cy.contains('Accordion').click()
+        cy.contains ('A nebula is an interstellar cloud of dust, hydrogen').should('be.hidden')
+        cy.contains('Toggle First Item').click({force:true});
+        cy.contains ('A nebula is an interstellar cloud of dust, hydrogen').should('be.visible')
         
-    // })
+    })
 
 
-    // it("third task", () => {
-    //     cy.visit ("/pages")
-    //     cy.get("[title = 'Modal & Overlays']").click()
-    //     cy.contains('Popover').click()
-    //     cy.contains('Right').trigger('mouseenter')
-    //     cy.get('[nbpopover]').should('be.visible', { timeout: 10000 })
-    //     cy.get('[nbpopover]').should('have.attr', 'nbpopover', 'Hello, how are you today?')
+    it("third task", () => {
+        cy.visit ("/pages")
+        cy.get("[title = 'Modal & Overlays']").click()
+        cy.contains('Popover').click()
+        cy.contains('Right').trigger('mouseenter')
+        cy.get('[nbpopover]').should('be.visible', { timeout: 10000 })
+        cy.get('[nbpopover]').should('have.attr', 'nbpopover', 'Hello, how are you today?')
       
-    // })
+    })
 
 
-    // it("fourth task", () => {
-    //     cy.visit ("/pages/modal-overlays/dialog")
-    //     cy.contains('Enter Name').click()
-    //     cy.get("[placeholder='Name']").type('Vika')
-    //     cy.contains('Submit').click()
-    //     // cy.contains('Enter Name').click()
-    //     // cy.get("[placeholder='Name']").type('Alex')
-    //     // cy.contains('Submit').click()
-    //     // cy.contains('Enter Name').click()
-    //     // cy.get("[placeholder='Name']").type('Ann')
-    //     //cy.contains('Submit').click()
-    //     cy.get('h3:contains("Names")').next('ul')
-    //     .find('li:contains("Vika")').should('exist'); 
-    // })
-    // 
+    it("fourth task", () => {
+        cy.visit ("/pages/modal-overlays/dialog")
+        cy.contains('Enter Name').click()
+        cy.get("[placeholder='Name']").type('Vika')
+        cy.contains('Submit').click()
+        // cy.contains('Enter Name').click()
+        // cy.get("[placeholder='Name']").type('Alex')
+        // cy.contains('Submit').click()
+        // cy.contains('Enter Name').click()
+        // cy.get("[placeholder='Name']").type('Ann')
+        //cy.contains('Submit').click()
+        cy.get('h3:contains("Names")').next('ul')
+        .find('li:contains("Vika")').should('exist'); 
+    })
+    
 }
     )
+
+    
