d3b4091c93faae5936592541536ee1833f389d33
vikakardakova <vika2893@gmail.com>
2023-10-31 19:34:07 +0200

    my first workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,26 @@
 const { defineConfig } = require("cypress");
+const { configurePlugin } = require("cypress-mongodb");
 
 module.exports = defineConfig({
   retries: 1,
-  reporter: 'mochawesome',
+  projectId: "d15px9", //for cypressCloud
+  reporter: "mochawesome",
   reporterOptions: {
-    reportDir: 'cypress/results',
+    reportDir: "cypress/results",
     overwrite: false,
     html: false,
-    json: true,}
-    ,
+    json: true,
+  },
+  env: {
+    mongodb: {
+      uri: "mongodb://localhost:27017",
+      database: "database_name",
+      collection: "collection_name",
+    },
+  },
   e2e: {
     setupNodeEvents(on, config) {
+      configurePlugin(on);
     },
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4200/",
