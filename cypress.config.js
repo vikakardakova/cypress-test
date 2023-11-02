5657049c183608645fb45e41cd0923bc35b9c489
vikakardakova <vika2893@gmail.com>
2023-11-02 16:22:24 +0200

    new

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require("cypress");
-const { configurePlugin } = require("cypress-mongodb");
-
 module.exports = defineConfig({
   retries: 1,
   //projectId: "d15px9", for cypressCloud
@@ -11,16 +9,8 @@ module.exports = defineConfig({
     html: false,
     json: true,
   },
-  env: {
-    mongodb: {
-      uri: "mongodb://localhost:27017",
-      database: "database_name",
-      collection: "collection_name",
-    },
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      configurePlugin(on);
     },
     specPattern: "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4200/",
